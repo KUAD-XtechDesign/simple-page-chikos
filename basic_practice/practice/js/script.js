2d048449ec9eea2b5dcabaa56f03bd53a8e71916
濱口琴音 <chiko@hamaguchikotonenoMacBook-Pro.local>
2020-09-28 10:14:56 +0900

    ooo

--- basic_practice/practice/js/script.js
+++ basic_practice/practice/js/script.js
@@ -1,14 +1,10 @@
 
 $(function(){
 
-    
+    $(".btn").on("click",()=>{
+        $("#slide").slideToggle();
+    })
 
     $("#slide").slick({dots:true});
-    $('.autoplay').slick({
-        slidesToshow: 1,
-        slidesToscroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-    });
 
 });

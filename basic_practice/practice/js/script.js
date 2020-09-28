d30f449b9e7001322c73362787e3d3697b0d37fa
濱口琴音 <chiko@hamaguchikotonenoMacBook-Pro.local>
2020-09-28 10:13:20 +0900

    iii

--- basic_practice/practice/js/script.js
+++ basic_practice/practice/js/script.js
@@ -6,5 +6,11 @@ $(function(){
     })
 
     $("#slide").slick({dots:true});
+    $('.autoplay').slick({
+        slidesToshow: 1,
+        slidesToscroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 
 });
